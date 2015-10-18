3349b323fb35d96ed341a83a989023d00b087d6f
trevor-hingley <trevor.hingley@oxfordcc.co.uk>
2015-10-18 13:21:11 +0100

    Initial commit.

---
+++ OxfordCC.ContrOCC.SQLUtilities.Presentation/wMDI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OxfordCC.ContrOCC.SQLUtilities.Presentation
+{
+	public partial class wMDI : Form
+	{
+		public wMDI()
+		{
+			InitializeComponent();
+		}
+	}
+}
