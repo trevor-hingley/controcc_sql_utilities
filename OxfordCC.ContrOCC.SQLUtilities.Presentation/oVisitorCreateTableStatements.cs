bb818231c8ae3fc0073ef1f03e076b9075122734
trevor-hingley <trevor.hingley@oxfordcc.co.uk>
2015-10-20 21:21:47 +0100

    Added untracked class. Changed target framework version to more commonly used 4.5.

---
+++ OxfordCC.ContrOCC.SQLUtilities.Presentation/oVisitorCreateTableStatements.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace OxfordCC.ContrOCC.SQLUtilities.Presentation
+{
+	public class oVisitorCreateTableStatements : oVisitorBase
+	{
+		public override void ExplicitVisit(CreateTableStatement fragment) { _fragments.Add(fragment); }
+	}
+}
